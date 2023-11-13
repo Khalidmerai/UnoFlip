f11c06796624f8058f1aaffe064ed297e0719a0e
marinalatif1 <114357108+marinalatif1@users.noreply.github.com>
2023-11-12 19:07:52 -0500

    Update UnoGame.java

--- SRC/UnoGame.java
+++ SRC/UnoGame.java
@@ -345,4 +345,8 @@ public class UnoGame {
     public ArrayList<Player> getPlayers() {
         return players;
     }
+    public ArrayList<Card> getCurrentPlayerCards() {
+        Player currentPlayer = getCurrentPlayer();
+        return currentPlayer.getCards();
+    }
 }
